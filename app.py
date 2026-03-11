2446a1688677f57a45ee2056e284c63850a63988
Dheeraj Kumar <dheerajwork657@gmail.com>
2026-03-11 16:26:00 +0530

    Fix emoji encoding

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Python Flask CI/CD on AWS 🚀"
+    return "<h1>Docker CI/CD Deployed Successfully 🚀</h1><p>Docker + GitHub Actions + AWS EC2</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
